4e03f101955b1c691cade380b9fa9f7e6f844682
Shuduo Sang <sangshuduo@gmail.com>
2023-10-18 21:34:46 -0400

    feat: add plotext

--- forecast.py
+++ forecast.py
@@ -4,6 +4,7 @@ import lightgbm as lgb
 import matplotlib.pyplot as plt
 import mlforecast
 import pandas as pd
+import plotext
 from mlforecast.target_transforms import Differences
 from sklearn.linear_model import LinearRegression
 from sqlalchemy import create_engine, text
@@ -16,6 +17,12 @@ if __name__ == "__main__":
         "--dump", type=str, help="dump forecast picture to file", required=False
     )
 
+    parser.add_argument(
+        "--plotext",
+        action="store_true",
+        help="plot forecast picture in console mode",
+        required=False,
+    )
     args = parser.parse_args()
 
     engine = create_engine("taos://root:taosdata@localhost:6030/power")
@@ -54,16 +61,20 @@ if __name__ == "__main__":
     predicts = forecast.predict(52)
 
     if not args.dump:
-        fig = plt.figure()
-        timer = fig.canvas.new_timer(interval=5000)
-        timer.add_callback(plt.close)
-        df.set_index("ds").plot(
-            figsize=(12, 8), title="current data, will disappear in 5 sec"
-        )
+        if args.plotext:
+            plotext.scatter(df)
+            plotext.show()
+        else:
+            fig = plt.figure()
+            timer = fig.canvas.new_timer(interval=5000)
+            timer.add_callback(plt.close)
+            df.set_index("ds").plot(
+                figsize=(12, 8), title="current data, will disappear in 5 sec"
+            )
 
-        timer.start()
-        plt.show()
-        timer.stop()
+            timer.start()
+            plt.show()
+            timer.stop()
 
     pd.concat([df, predicts]).set_index("ds").plot(
         figsize=(12, 8), title="current data with forecast"
@@ -71,6 +82,8 @@ if __name__ == "__main__":
 
     if args.dump:
         plt.savefig(args.dump)
+    elif args.plotext:
+        plotext.show()
     else:
         plt.show()
     print("Done")
